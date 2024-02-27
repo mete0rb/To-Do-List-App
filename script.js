251d5ed3b286855ef81fee064015e0bbea2097bf
mete0rb <153194582+mete0rb@users.noreply.github.com>
2024-02-27 18:45:54 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form');
+const list = document.getElementById('list');
+
+form.addEventListener('submit', addTask);
+
+function addTask(e) {
+  e.preventDefault();
+
+  // add new task to list
+}
+
+// toggle completed tasks
+// remove tasks
+// persist data to localStorage
